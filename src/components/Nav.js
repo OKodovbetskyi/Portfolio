239321225a5d4eecd9ce34ebf0e2aa4dbba3ee87
OKodovbetskyi <akodovbetskiy@outlook.com>
2023-06-05 13:12:50 +0100

    Added mobile menu for mobile versions

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,14 +5,10 @@ import logo from '../assets/ak-low-resolution-logo-black-on-white-background.png
 import Skills from './Skills';
 import Projects from './Projects';
 import { Link } from 'react-router-dom';
+import { Contact } from './Contact';
 const Nav = (props) =>{
    const [isOpen, setIsOpen] = useState(false);
-   const [selected, setSelected] = useState({
-      Home: true,
-      Skills: false,
-      Projects: false,
-      Education: false,
-    })
+
     const scrollToSection = (elRef) => {
          const scrollLocation ={
             top: (elRef.current.offsetTop)-100,
@@ -20,16 +16,14 @@ const Nav = (props) =>{
         }
         window.scrollTo(scrollLocation);
     }
-    const selectHandler = (name)=>{
-      setSelected({[name] : true})
-    }
     return(
+      <>
      <nav className="navbar">
       <Link to="/"><img className="nav-logo" src={logo} /></Link>
       <div className="navbar-links">
-        <Link onClick={()=>selectHandler('Projects')} className={selected.Projects && 'selected'} to="/projects">Projects</Link>
-        <Link onClick={()=>selectHandler('Education')} className={selected.Education && 'selected'} to="/education">About</Link>
-        <Link onClick={()=>selectHandler('Education')} className={selected.Education && 'selected'} to="/education">Contact</Link>
+      <Link   to="/about">About</Link>
+      <Link   to="/projects">Projects</Link>
+      <Link to="/contact-me">Contact</Link>
       </div>
       <div className="navbar-hamburger" onClick={() => setIsOpen(!isOpen)}>
         <div className={`navbar-hamburger-line ${isOpen ? 'open' : ''}`}></div>
@@ -38,8 +32,29 @@ const Nav = (props) =>{
         <div className={`navbar-hamburger-line ${isOpen ? 'open' : ''}`}></div>
       </div>
   </nav>
+  {isOpen ? <>
+    <div className='mobile-menu' >
+     
+   </div>
+       <div className='hamburger-menu'>
+       <div class="close-icon"  onClick={() => setIsOpen(!isOpen)}>
+       <div class="close-icon-circle">
+         <span class="close-icon-line"></span>
+         <span class="close-icon-line"></span>
+        </div>
+        </div>
+       <Link onClick={() => setIsOpen(!isOpen)} to="/">Home</Link>
+       <Link onClick={() => setIsOpen(!isOpen)} to="/about">About</Link>
+       <Link onClick={() => setIsOpen(!isOpen)} to="/projects">Projects</Link>
+       <Link onClick={() => setIsOpen(!isOpen)} to="/contact-me">Contact</Link>
+   </div>
+   </>
+  : ''
+   }
+  
+    </>
     )
-}
+  }
 export default Nav;
      /*<Link to="/">Home </Link>
         <Link to="#skills">Skills</Link>
