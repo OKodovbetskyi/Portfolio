f745284495a64a83324f77a5a7a6d30445b7525b
K2054852 <k2054852@kingston.ac.uk>
2023-06-28 12:39:37 +0100

    Fixed links

--- src/components/IconsLinks.js
+++ src/components/IconsLinks.js
@@ -13,14 +13,14 @@ const IconsLinks = ({icon,icon1, icon2, icon3}) =>{
   >
 
             <p className={styles.email}>Resume</p> <img src={icon} alt=''/></a></div>
-           <div className={styles.wrapper}><a href="mailto:oleksandr.kodovbetskyi@gmail.com"> 
+           <div className={styles.wrapper}><a href="mailto:oleksandr.kodovbetskyi@gmail.com" target="_blank"> 
            <p className={styles.email}>Email me!</p> <img src={icon2}alt='' />
            </a></div>
            <div className={styles.wrapper}>
-           <a href="https://github.com/OKodovbetskyi">
+           <a href="https://github.com/OKodovbetskyi" target="_blank">
             <p className={styles.email}>GitHub</p> <img src={icon1} alt=''/></a></div>
            <div className={styles.wrapper}>
-           <a href="https://www.linkedin.com/in/ak-developer">
+           <a href="https://www.linkedin.com/in/ak-developer" target="_blank" >
             <p className={styles.email}>Linkeln</p> <img src={icon3} alt=''/></a></div>
            
            
