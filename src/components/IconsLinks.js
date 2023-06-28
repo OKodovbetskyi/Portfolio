c9414d21a35a420725bf05bbacfcf4565486f6e4
K2054852 <k2054852@kingston.ac.uk>
2023-06-28 13:34:21 +0100

    resume from an external file

--- src/components/IconsLinks.js
+++ src/components/IconsLinks.js
@@ -1,10 +1,10 @@
 import React from "react";
-import resume from "../assets/Oleksandr_Kodovbetskyi_CV.pdf"
+
 import styles from "./Icons.module.css";
 const IconsLinks = ({icon,icon1, icon2, icon3}) =>{
     return (
         <span className={styles.Icons}>
-            <div className={styles.wrapper}> <a download href={resume}>
+            <div className={styles.wrapper}> <a download href="https://drive.google.com/file/d/12KVDCBMmGgFeqtxq2S8sNrXxZwgp_6gI/view?usp=sharing">
 
             <p className={styles.email}>Resume</p> <img src={icon} alt=''/></a></div>
            <div className={styles.wrapper}><a href="mailto:oleksandr.kodovbetskyi@gmail.com" target="_blank"> 
